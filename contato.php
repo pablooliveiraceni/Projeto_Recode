b1e13bfde45bd0a2def880f8dd3aeaaa1a7c1206
Pablo Ceni <Pablo Ceni>
2020-11-06 21:22:02 -0300

    Implementaçao do bootstrap

--- contato.php
+++ contato.php
@@ -2,7 +2,7 @@
 <?php 
 $servername = "localhost";
 $username   = "root";
-$password   = "senha*-*";
+$password   = "yuyuhakusho";
 $database   = "fseletro";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -18,8 +18,7 @@ if(isset($_POST['nome']) && isset($_POST['msg'])){
 
     $sql = "insert into comentarios (nome,  msg) values ('$nome', '$msg')";
 
-    $result = $conn->query($sql);
- 
+    $result = $conn->query($sql); 
 } 
 ?>
 
@@ -30,6 +29,8 @@ if(isset($_POST['nome']) && isset($_POST['msg'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contato - Full Stack Eletro</title>
     <link rel="stylesheet" href="CSS/estilo.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
     <!-- íncio Menu -->
@@ -37,52 +38,65 @@ if(isset($_POST['nome']) && isset($_POST['msg'])){
      include('menu.html');
     ?>
     <!-- Fim Menu -->
-
-    <header>
-        <h2>Contato</h2>
-    </header>
-    <hr>
     <main>
-        <section class="contatos">
-            <div class="contatos">
-                <img class="imgContatoEmail" src="img/email.png" alt="E-mail">
-                <label class="textContatoEmail">contato@fullstackeletro.com</label>
-                <br>
-                <img class="imgContatoWhats" src="img/telefoneWhatsapp.png" alt="Whatsapp">
-                <label class="textContatoWhats">(11) 99999-9999</label>
-            </div>
-        </section>  
-        <section class="formulario">
-            <form action="" method="post" >
-                <input type="text" name="nome" id="nome" placeholder="Nome" size="50" maxlength="40"> <br> <br>
-                <textarea name="msg" id="" cols="50" rows="10" placeholder="Escreva aqui sua mensagem"></textarea>  <br>
-                <input class="enviar" type="submit" value="Enviar">
-            </form>
-        </section>
+        <div container-fluid>
+            <header>
+                <h2>Contato</h2>
+            </header>
+            <hr>
+        
+            <section class="contatos">
+                <div class="contatos">
+                    <img class="imgContatoEmail" src="img/email.png" alt="E-mail">
+                    <label class="textContatoEmail">contato@fullstackeletro.com</label>
+                    <br>
+                    <img class="imgContatoWhats" src="img/telefoneWhatsapp.png" alt="Whatsapp">
+                    <label class="textContatoWhats">(11) 99999-9999</label>
+                </div>
+            </section>  
+            <section class="formulario bg-light">
+                
+                <form action="" method="post">
+                <div  class="form-group">
+                   <label for="nome">Nome:</label>
+                    <input type="text" name="nome" id="nome" placeholder="Nome" size="50" maxlength="40"> <br> <br>
+                </div>
+                <div class="form-group">
+                    <label for="mensagem">Mensagem</label> <br>
+                    <textarea name="msg" id="" cols="50" rows="10" placeholder="Escreva aqui sua mensagem"></textarea>  <br>
+                </div>
+                <button class="btn btn-danger btn" id="Enviar" type="submit" value="Enviar">Enviar</button>
+                </form>
+            </section>
+
+        </div>
+        
+        <?php
+        
+        $sql ="select * from comentarios";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0) {
+            while($rows = $result->fetch_assoc()){    
+                echo "Data: ", $rows['data'], "<br/>";    
+                echo "Nome: ", $rows['nome'], "<br/>";  
+                echo "Mensagem: ", $rows['msg'], "<br/>";  
+                echo "<hr/>";
+
+        }
+        }else {
+            echo "Nenhum comentário foi digitado";
+        }
+
+        ?>
     </main>
-    <?php
-    
-    $sql ="select * from comentarios";
-    $result = $conn->query($sql);
-
-    if($result->num_rows > 0) {
-        while($rows = $result->fetch_assoc()){    
-            echo "Data: ", $rows['data'], "<br/>";    
-            echo "Nome: ", $rows['nome'], "<br/>";  
-            echo "Mensagem: ", $rows['msg'], "<br/>";  
-            echo "<hr/>";
-
-    }
-    }else {
-        echo "Nenhum comentário foi digitado";
-    }
-
-     ?>
     <footer id="rodape">
         <p id="formas_pagamento">Formas de pagamento</p>
         <img src="img/formasPagamento.png" alt="Formas de Pagamento">
         <p>&copy; Recode Pro</p>
      </footer> 
-     
+    
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
--- produtos.php
+++ produtos.php
@@ -1,7 +1,7 @@
 <?php 
 $servername = "localhost";
 $username   = "root";
-$password   = "senha*-*";
+$password   = "yuyuhakusho";
 $database   = "fseletro";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -21,6 +21,8 @@ if(!$conn){
     <title>Produtos - Full Stack Eletro</title>
     <link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
     <script src="js/funcoes.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
      <!-- íncio Menu -->
@@ -30,61 +32,65 @@ if(!$conn){
     <!-- Fim Menu -->
 
     <main>
-        <header>
-            <h2>Produtos</h2>
-        </header>
-        <hr>
-            <!-- Categorias-->
+        <div class="container-fluid">
+            <header>
+                <h2>Produtos</h2>
+            </header>
+            <hr>
+                <!-- Categorias-->
 
-          <section class="categorias"> 
-              <aside>
-                <h3>Categorias</h3>
-                    <ul style="list-style: none;">
-                    <li onclick="exibir_todos()" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Todos (12)</li>
-                    <li onclick="exibir_categoria('geladeira')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Geladeira (3)</li>
-                    <li onclick="exibir_categoria('fogao')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Fogões (2)</li>
-                    <li onclick="exibir_categoria('microondas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Microondas (3)</li>                     
-                    <li onclick="exibir_categoria('lavadouraderoupas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Lavadoura de roupas (2)</li>                     
-                    <li onclick="exibir_categoria('lavaloucas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Lava louças (2)</li>                     
-                   </ul>
-              </aside>               
-            </section> 
-             <!-- Produtos-->   
+            <section class="categorias"> 
+                <aside>
+                    <h3>Categorias</h3>
+                        <ul style="list-style: none;">
+                        <li onclick="exibir_todos()" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Todos (12)</li>
+                        <li onclick="exibir_categoria('geladeira')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Geladeira (3)</li>
+                        <li onclick="exibir_categoria('fogao')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Fogões (2)</li>
+                        <li onclick="exibir_categoria('microondas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Microondas (3)</li>                     
+                        <li onclick="exibir_categoria('lavadouraderoupas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Lavadoura de roupas (2)</li>                     
+                        <li onclick="exibir_categoria('lavaloucas')" onmouseover="aumentarMenu(this)" onmouseout="diminuirMenu(this)">Lava louças (2)</li>                     
+                    </ul>
+                </aside>               
+                </section> 
+                <!-- Produtos-->   
 
-             
-            <?php
-              $sql = "select * from produtos";
+                
+                <?php
+                $sql = "select * from produtos";
 
-              $result = $conn->query($sql);
-              
-              
-              if($result->num_rows > 0){
-                  while($rows = $result->fetch_assoc()){
-             ?>
+                $result = $conn->query($sql);
+                
+                
+                if($result->num_rows > 0){
+                    while($rows = $result->fetch_assoc()){
+                ?>
 
-                <div class="box_produto" id="<?php echo $rows["categoria"];?>" >
-                    <img src="<?php echo $rows["imagem"];?>" width="40%" onmouseover="aumentar(this)" onmouseout="diminuir(this)"> 
-                    <br>
-                    <p class="descricao"><?php echo $rows["descricao"];?></p>
-                    <hr>
-                    <p class="descricao"><strike><?php echo $rows["preco"];?></strike></p>
-                    <p class="preco"><?php echo $rows["preco_final"];?></p>
-                </div>
-             <?php
-                  }
-              
-              }else{
-                  echo "Nenhum produto cadastrado";
-              }
-            
-            ?>        
-            
+                    <div class="box_produto" id="<?php echo $rows["categoria"];?>" >
+                        <img src="<?php echo $rows["imagem"];?>" width="40%" onmouseover="aumentar(this)" onmouseout="diminuir(this)"> 
+                        <br>
+                        <p class="descricao"><?php echo $rows["descricao"];?></p>
+                        <hr>
+                        <p class="descricao"><strike><?php echo $rows["preco"];?></strike></p>
+                        <p class="preco"><?php echo $rows["preco_final"];?></p>
+                    </div>
+                <?php
+                    }
+                
+                }else{
+                    echo "Nenhum produto cadastrado";
+                }
+                
+                ?>        
+
+        </div>      
     </main>
     <footer id="rodape">
         <p id="formas_pagamento">Formas de pagamento</p>
             <img width="300" height="100" src="img/formasPagamento.png" alt="Formas de Pagamento">
         <p>&copy; Recode Pro</p>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 </body>
 </html>
