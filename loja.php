1526ee07fcfe157e7090856554ce89783f4a7478
Pablo Ceni <Pablo Ceni>
2020-11-06 21:32:22 -0300

    Implementaçao do bootstrap

--- loja.php
+++ loja.php
@@ -15,41 +15,53 @@
     <!-- Fim Menu -->
     <!-- Fim Menu -->
     <main>
+        <div class="container-fluid">
             <header>
-                <h2>Nossas Lojas</h2>
+                <h1>Nossas Lojas</h1>
             </header>
-        <hr>
-        <section>
-            <div class="cidade">
-                <ul>
-                    <li><h3>Rio de Janeiro</h3></li>
-                    <li><p>Avenida Presidente Vargas, 5000</p></li>
-                    <li><p>10 &ordm; andar</p></li>
-                    <li><p>Centro</p></li>
-                    <li><p>(21) 3333-3333</p></li>
-                </ul>
-            </div>
-            <div class="cidade">
+            
+            <hr>
+            <section class="lojas">
+                <table class="table">
+                  <thead class="thead-light">
+                    <tr>
+                        <th scope="col">Cidade</th>
+                        <th scope="col">Endereço</th>
+                        <th scope="col">Número</th>
+                        <th scope="col">Proximidade</th>
+                        <th scope="col">Telefone</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Rio de Janeiro</th>
+                            <td>Avenida Presidente Vargas, 5000</td>
+                            <td>10 &ordm; andar</td>
+                            <td>Centro</td>
+                            <td>(21) 3333-3333</td>
+                        </tr>
 
-                <ul>
-                    <li><h3>São Paulo</h3></li>
-                    <li><p>Avenidade Paulista, 985</p></li>
-                    <li><p>3 &ordm; andar</p></li>
-                    <li><p>Jardins</p></li>
-                    <p>(11) 4444-4444</p>
-                </ul>
-            </div>
-            <div class="cidade">
-                <ul>
-                    <li><h3>Santa Catarina</h3></li>
-                    <li><p>Rua Major &Aacute; vila, 370</p></li>
-                    <li><p>Vila Mariana</p></li>
-                    <p>(47) 5555-5555</p>
-                </ul>
-            </div>
-        </section>
+                        <tr>
+                            <th scope="row">São Paulo</th>
+                            <td>Avenidade Paulista, 985</td>
+                            <td>3 &ordm; andar</td>
+                            <td>Jardins</td>
+                            <td>(11) 4444-4444</td>
+                        </tr>
+                        
+                        <tr>
+                            <th scope="row">Santa Catarina</th>
+                            <td>Rua Major &Aacute; vila, 370</td>
+                            <td>5 &ordm; andar</td>
+                            <td>Nordeste</td>
+                            <td>(47) 5555-5555</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </div>
     </main>
-    <br><br><br><br><br>
+
     <footer id="rodape">
         <p id="formas_pagamento">Formas de pagamento</p>
         <img src="img/formasPagamento.png" alt="Formas de Pagamento">
